e45491820432079c4d25dd97c8ba2596f82a5482
Abhishek Jaiswal <43906801+Abhi590@users.noreply.github.com>
2020-07-27 11:56:27 +0200

    Update CustomScript.ps1

--- CustomScript.ps1
+++ CustomScript.ps1
@@ -1,7 +1,7 @@
-powershell.exe Install-WindowsFeature -name Web-Server -IncludeManagementTools
-powershell.exe Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-powershell.exe choco install rabbitmq cup all -y
-powershell.exe docker pull mcr.microsoft.com/dotnet/core/samples:aspnetapp
-powershell.exe docker run -d --rm -p 5000:80 --name aspnetcore_sample mcr.microsoft.com/dotnet/core/samples:aspnetapp
-powershell.exe remove-item 'C:\\inetpub\\wwwroot\\iisstart.htm'
-powershell.exe Add-Content -Path 'C:\\inetpub\\wwwroot\\iisstart.htm' -Value $('Hello World from ' + $env:computername)
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+remove-item 'C:\inetpub\wwwroot\iisstart.htm' -confirm:$false -Force
+Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value $('Hello World from ' + $env:computername)
+docker pull mcr.microsoft.com/dotnet/core/samples:aspnetapp
+docker run -d --rm -p 5000:80 --name aspnetcore_sample mcr.microsoft.com/dotnet/core/samples:aspnetapp
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install rabbitmq cup all -y
