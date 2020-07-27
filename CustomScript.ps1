e5bc8de0a4acc2048d56ddd9dae1832a19752f94
Abhishek Jaiswal <43906801+Abhi590@users.noreply.github.com>
2020-07-27 13:27:18 +0200

    Update CustomScript.ps1

--- CustomScript.ps1
+++ CustomScript.ps1
@@ -5,3 +5,5 @@ docker pull mcr.microsoft.com/dotnet/core/samples:aspnetapp
 docker run -d --rm -p 5000:80 --name aspnetcore_sample mcr.microsoft.com/dotnet/core/samples:aspnetapp
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install rabbitmq cup all -y
+cd 'c:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.5\sbin'
+.\rabbitmq-plugins enable rabbitmq_management
