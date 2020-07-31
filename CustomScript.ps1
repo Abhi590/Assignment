6373576d6320d6dd4c171b61a84d790167550d59
Abhishek Jaiswal <43906801+Abhi590@users.noreply.github.com>
2020-07-31 09:30:51 +0200

    Update CustomScript.ps1

--- CustomScript.ps1
+++ CustomScript.ps1
@@ -2,8 +2,8 @@ Install-WindowsFeature -name Web-Server -IncludeManagementTools
 remove-item 'C:\inetpub\wwwroot\iisstart.htm' -confirm:$false -Force
 Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value $('Hello World from ' + $env:computername)
 docker pull mcr.microsoft.com/dotnet/core/samples:aspnetapp
-docker run -d --rm -p 5000:80 --name aspnetcore_sample mcr.microsoft.com/dotnet/core/samples:aspnetapp
+# docker run -d --rm -p 5000:80 --name aspnetcore_sample mcr.microsoft.com/dotnet/core/samples:aspnetapp
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install rabbitmq cup all -y
-cd 'c:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.5\sbin'
-.\rabbitmq-plugins enable rabbitmq_management
+# choco install rabbitmq cup all -y
+# cd 'c:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.5\sbin'
+# .\rabbitmq-plugins enable rabbitmq_management
